7f4970dd240669da442dbf6a2dc41b834d4795da
Ben <26501287+Xarithma@users.noreply.github.com>
2024-01-01 05:27:26 +0100

    Added plugin "main menu"

--- addons/easy_godot_features/plugin.gd
+++ addons/easy_godot_features/plugin.gd
@@ -1,14 +1,37 @@
 @tool
 extends EditorPlugin
 
-var dock
+const MainPanel = preload("res://addons/easy_godot_features/main_menu.tscn")
+
+var main_panel_instance
 
 
-func _enter_tree() -> void:
-	dock = preload("res://addons/easy_godot_features/dock.tscn").instantiate()
-	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
+func _enter_tree():
+	main_panel_instance = MainPanel.instantiate()
+	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+
+	# ! Hide the main panel. Very much required.
+	_make_visible(false)
 
 
 func _exit_tree() -> void:
-	remove_control_from_docks(dock)
-	dock.free()
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+
+func _has_main_screen() -> bool:
+	return true
+
+
+func _make_visible(visible) -> void:
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+func _get_plugin_name() -> String:
+	return "Easy Godot Features"
+
+
+func _get_plugin_icon() -> Texture2D:
+	# ! Must return some kind of Texture for the icon.
+	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
